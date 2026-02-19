af2734f0d0bb2209fef1d0e909e01bffee0e3034
PavDev3 <pnunfe@gmail.com>
2026-02-19 23:12:32 +0100

    Fix: App component was not properly exported

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { Home } from './components/home/home';
-import { TicTacToe } from './components/tic-tac-toe/tic-tac-toe';
-import { MemoryGame } from './components/memory-game/memory-game';
-import { SnakeGame } from './components/snake-game/snake-game';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-export default {
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, Home, TicTacToe, MemoryGame, SnakeGame]
-};
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
+})
+export class App {}
